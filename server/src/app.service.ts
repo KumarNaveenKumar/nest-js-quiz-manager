ff73d15cc50dd0e8db7253b081e7be6ab4cfa953
KumarNaveenKumar <naveenchaudahryyyy@gmail.com>
2022-11-09 15:17:07 +0530

    init project

---
+++ server/src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+  
+  getSomething(): string {
+    return 'Something can be sent back';
+  }
+}
